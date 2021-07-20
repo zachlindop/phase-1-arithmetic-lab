bcf44dca21feb5d00b6ad82065da89e6f9ef0d23
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-19 20:08:55 -0700

    commit tree b50612087e39e1cd07473b3d3f6ef4d829b8d796

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Number.isInteger(55) + 1;
+
+let num3 = 16
+let num4 = 6
+
+const mod = num3 %= num4;
+
+const max = Math.max(5, 10, 15, 20);
